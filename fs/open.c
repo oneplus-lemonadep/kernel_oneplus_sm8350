31c1fab6e5b0df9780635ab7f642bf4edddf0e47
xiaoka6666 <115566356+xiaoka6666@users.noreply.github.com>
2026-01-16 23:17:32 +0800

    feat(kernel): 添加KernelSU支持及相关系统调用钩子
    添加KernelSU配置选项并在关键系统调用路径添加钩子函数
    包括reboot、input、stat、faccessat和execve等系统调用

--- fs/open.c
+++ fs/open.c
@@ -345,6 +345,10 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
  * We do this by temporarily clearing all FS-related capabilities and
  * switching the fsuid/fsgid around to the real ones.
  */
+#ifdef CONFIG_KSU
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
+			 int *flags);
+#endif
 
 #ifdef CONFIG_KSU_SUSFS_SUS_SU
 extern bool susfs_is_sus_su_hooks_enabled __read_mostly;
@@ -361,6 +365,9 @@ long do_faccessat(int dfd, const char __user *filename, int mode)
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
 
+#ifdef CONFIG_KSU
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+#endif
 #ifdef CONFIG_KSU_SUSFS_SUS_SU
 	if (susfs_is_sus_su_hooks_enabled) {
 		ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
